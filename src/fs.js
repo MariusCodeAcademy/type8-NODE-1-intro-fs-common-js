c92695fee7a47ffa923060edd3b83f64a1e124e3
Marius <marius@example.com>
2022-04-06 10:30:32 +0300

    delete

--- src/fs.js
+++ src/fs.js
@@ -3,12 +3,37 @@ const fs = require('fs');
 // console.log('fs ===', fs);
 
 // fs.writeFile('failo pavadinimas su keliu', 'duomenys i faila', callback funkcija su klaidos objektu)
-fs.writeFile('../dist/style.css', 'Username=James\nage=25', (err) => {
-  if (err) {
-    console.warn(err);
-    return;
-  }
-  console.log('failas sukurtas');
-});
-
+function write() {
+  fs.writeFile('../dist/style.css', 'Username=James\nage=25', (err) => {
+    if (err) {
+      console.warn(err);
+      return;
+    }
+    console.log('failas sukurtas');
+  });
+}
+// write();
 // read files
+const read = () => {
+  fs.readFile('./new.txt', (err, data) => {
+    if (err) {
+      console.warn(err);
+      return;
+    }
+    console.log('data ===', data.toString());
+  });
+};
+
+// delete
+if (fs.existsSync('./deleteMe.txt')) {
+  console.log('fileFound');
+  fs.unlink('./deleteMe.txt', (err) => {
+    if (err) {
+      console.warn(err);
+      return;
+    }
+    console.log('Files istrintas');
+  });
+} else {
+  console.log('file Not Found');
+}
