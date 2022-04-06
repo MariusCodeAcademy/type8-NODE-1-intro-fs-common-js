f429968398df96c43f2e6483f7c22ac829b10700
Marius <marius@example.com>
2022-04-06 11:01:02 +0300

    casual

---
+++ src/casual.js
@@ -0,0 +1,9 @@
+// import casual
+const casual = require('casual');
+
+const sent1 = casual.sentence;
+
+console.log('sent1 ===', sent1);
+
+// funkcijos generateAddress() pagalba sugeneruoti random adressa is casual
+// funkcija turetu grazinti objekta kuris turi country, city, state ,  address
